4188eef78a87521a64dc757d9746b12ef9af5625
Thiago Candido <thiagocarvalhocandido8@gmail.com>
2025-07-17 14:17:04 -0300

    Add project files.

---
+++ Enums/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioSecao8.Enums
+{
+    enum OrderStatus : int
+    {
+        PENDING_PAYMENT = 0,
+        PROCESSING = 1,
+        SHIPPED = 2,
+        DELIVERED = 3
+    }
+}
